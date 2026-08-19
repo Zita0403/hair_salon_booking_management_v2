6d2f621856ebdd22216d49c81e7a434aa98dec82
Zita0403 <zitalukacs7@gmail.com>
2026-08-19 12:21:25 +0200

    Fix dotenv configuration by specifying the path to the .env file

--- app/api.js
+++ app/api.js
@@ -10,7 +10,6 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
 dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const db = new pg.Client({
